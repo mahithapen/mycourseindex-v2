a85c64924225fc5ebe2ce860e448a386d154d3f0
eys35 <eys35@cornell.edu>
2024-12-08 14:26:12 -0500

    changed names

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,18 +11,18 @@ function App() {
 
   const courses = [
     { value: '', label: 'Select a course...' },
-    { value: 'ARCH 1901', label: 'ARCH 1901 FWS: Topics in Architecture ' },
-    { value: 'ARTH 1100', label: 'ARTH 1100 Art Histories: An Introduction' },
-    { value: 'ASIAN 1111 ', label: 'ASIAN 1111 FWS: Literature, Culture, Religion ' },
+    // { value: 'ARCH 1901', label: 'ARCH 1901 FWS: Topics in Architecture ' },
+    // { value: 'ARTH 1100', label: 'ARTH 1100 Art Histories: An Introduction' },
+    { value: 'ASIAN 1111 FWS: Literature, Culture, Religion (2023FA) ', label: 'ASIAN 1111 FWS: Literature, Culture, Religion ' },
     { value: 'CS 3780 COMBINED-COMEET Introduction to Machine Learning (2024FA)', label: 'CS 3780 Introduction to Machine Learning ' },
     { value: 'ENGRD 2700 Basic Engineering Probability and Statistics (2024SP)', label: 'ENGRD 2700 Basic Engineering Probability and Statistics ' },
-    { value: 'ENGRG 1050', label: 'ENGRG 1050 Engineering Seminar ' },
-    { value: 'ENGRG 1400', label: 'ENGRG 1400 Engineering Project Team Onboarding ' },
-    { value: 'ENGRI 1270', label: 'ENGRI 1270 Introduction to Entrepreneurship for Engineers ' },
-    { value: 'Introduction to Analysis of Algorithms', label: 'CS 4820 Introduction to Analysis of Algorithms ' },
-    { value: 'MATH 2930', label: 'MATH 2930 Differential Equations for Engineers ' },
-    { value: 'MATH 2940', label: 'MATH 2940 Linear Algebra for Engineers ' },
-    { value: 'ORIE 3500', label: 'ORIE 3500 Eng Probability and Statistics: Modeling and Data Science II ' },
+    { value: 'ENGRG 1050 Engineering Seminar (2023FA)', label: 'ENGRG 1050 Engineering Seminar ' },
+    // { value: 'ENGRG 1400', label: 'ENGRG 1400 Engineering Project Team Onboarding ' },
+    // { value: 'ENGRI 1270', label: 'ENGRI 1270 Introduction to Entrepreneurship for Engineers ' },
+    // { value: 'Introduction to Analysis of Algorithms', label: 'CS 4820 Introduction to Analysis of Algorithms ' },
+    // { value: 'MATH 2930', label: 'MATH 2930 Differential Equations for Engineers ' },
+    // { value: 'MATH 2940', label: 'MATH 2940 Linear Algebra for Engineers ' },
+    // { value: 'ORIE 3500', label: 'ORIE 3500 Eng Probability and Statistics: Modeling and Data Science II ' },
     { value: 'PHYS 1110 Introduction to Experimental Physics (2023FA)', label: 'PHYS 1110 Introduction to Experimental Physics ' }
   ];
 
